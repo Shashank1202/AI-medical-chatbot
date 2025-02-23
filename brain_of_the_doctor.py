ca252f0684fcdddea70efc391d673aa8fbc19f9c
Shashank S <shashank.s.1202@gmail.com>
2025-02-24 01:35:08 +0530

    Project completed

---
+++ brain_of_the_doctor.py
@@ -0,0 +1,46 @@
+import os
+GROQ_API_KEY= os.getenv('GROQ_API_KEY')
+
+
+import base64
+
+
+def encode_image(image_path):
+    #image_path="acne.jpg"
+    image_file= open(image_path, "rb")
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+
+
+from groq import Groq
+query="Is there soemthing with my face?"
+model= "llama-3.2-90b-vision-preview"
+
+def analyze_image_with_query(query, model, encoded_image):
+
+    client= Groq()
+
+    messages=[
+        {
+            "role": "user",
+            "content":[
+                {
+                    "type": "text",
+                    "text": query
+                },
+                {
+                    "type":"image_url",
+                    "image_url":{
+                        "url":f"data:image/jpeg;base64,{encoded_image}",
+                    },
+                },
+            ],
+        }
+    ]
+
+    chat_completion= client.chat.completions.create(
+        messages= messages,
+        model= model
+    )
+
+    return (chat_completion.choices[0].message.content)
---
+++ voice_of_docter.py
@@ -0,0 +1,56 @@
+#Step1: Setup Text to Speech-TTS-model (gTTS & ElevenLabs)
+import os
+from gtts import gTTS
+import platform
+import subprocess
+from pydub import AudioSegment
+
+def text_to_speech_with_gtts(input_text, output_filepath):
+    language="en"
+
+
+    audioobj= gTTS(
+        text= input_text,
+        lang= language,
+        slow= False
+
+    )
+
+    audioobj.save(output_filepath)
+
+
+#input_text="Hi this is Ai"
+#text_to_speech_with_gtts(input_text=input_text, output_filepath="gtts_testing.mp3")
+
+#Step2: Use Model for Text output to Voice
+
+import subprocess
+import platform
+
+def text_to_speech_with_gtts(input_text, output_filepath):
+    language="en"
+
+
+    audioobj= gTTS(
+        text= input_text,
+        lang= language,
+        slow= False
+
+    )
+    audioobj.save(output_filepath)
+    os_name= platform.system()
+    try:
+        if os_name == "Darwin":  # macOS
+            subprocess.run(['afplay', output_filepath])
+        elif os_name == "Windows":  # Windows
+            subprocess.run(['powershell', '-c', f'(New-Object Media.SoundPlayer "{output_filepath}").PlaySync();'])
+        elif os_name == "Linux":  # Linux
+            subprocess.run(['aplay', output_filepath])  # Alternative: use 'mpg123' or 'ffplay'
+        else:
+            raise OSError("Unsupported operating system")
+    except Exception as e:
+        print(f"An error occurred while trying to play the audio: {e}")
+
+
+input_text="Hi this is Ai, autoplay testing!" 
+text_to_speech_with_gtts(input_text=input_text, output_filepath="gtts_testing_autoplay.mp3")
